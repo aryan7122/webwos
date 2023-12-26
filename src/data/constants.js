f1b032a01880c3d14f5e87716bd7d508124edb60
Aryan <aryankushwahalifenote@gmail.com>
2023-12-26 22:17:24 +0530

    V1.1.11

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,11 +2,10 @@
 export const Bio = {
   name: "Aryan Kushwaha",
   roles: [
-    "Full Stack Developer",
-    // "React Native Developer",
     "MERN Developer",
     "React js Developer",
-    // "UI/UX Designer",
+    "Node js Developer",
+    "UI/UX Designer",
     "Programmer",
   ],
   description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
