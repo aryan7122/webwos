70fdce81770deb75b7f11f6081cd81fdbf288056
Aryan <aryankushwahalifenote@gmail.com>
2023-12-26 09:55:34 +0530

    V 1.1.0.8

--- src/components/Navbar/NavbarStyledComponent.js
+++ src/components/Navbar/NavbarStyledComponent.js
@@ -134,11 +134,11 @@ export const MobileMenu = styled.div`
     width: 100%;
     padding: 12px 40px 24px 40px;
     background: ${({ theme }) => theme.card_light+99};
-    transition: all 0.6s ease-in-out;
+    transition: all 0.4s ease-in-out;
     transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
     border-radius: 0 0 20px 20px;
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+    opacity: ${({ isOpen }) => (isOpen ? '100%' : '1')};
     z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
 
 `
