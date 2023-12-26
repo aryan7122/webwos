24430d7cd9d48e839bacd551a168165c3f8d4652
Aryan <aryankushwahalifenote@gmail.com>
2023-12-26 22:46:58 +0530

    V1.1.11

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
   ],
   description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/aryan7122",
-  resume: "https://drive.google.com/file/d/1-KR1h6FAbO-AUhY6c8xwfaP9ShUDW9r1/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/1j1DcPddGRu5HSBWVuLoYSyUrSmGMA21R/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/aryan7122/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
